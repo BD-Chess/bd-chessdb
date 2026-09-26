71f015516ea9847d15ad9af34f18298f0c07b520
Bojan Dobrečevič <bd@siol.net>
2026-09-26 22:55:18 +0200

    Center ChessBest desktop layout across CURRENT LAB and PWA (#38)

--- public/chess/PWA/sw.js
+++ public/chess/PWA/sw.js
@@ -1,6 +1,6 @@
 /* PWA-only, release-consistent cache. Online APIs and credentials are excluded. */
 'use strict';
-const RELEASE = "20260926-d190b5431fc9";
+const RELEASE = "20260926-ca0977cfbef6";
 const ASSETS = [
   "8zc-about.html",
   "8zc-help.html",
